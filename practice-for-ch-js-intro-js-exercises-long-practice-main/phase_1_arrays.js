9c7b7f430a23b8d17f3dbd9ea6a0b5545d13079d
nj <nickgentrymma@gmail.com>
2023-05-16 16:18:53 -0700

    got almost through phase 1

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
@@ -1,5 +1,5 @@
 // Your code here
-console.log("message")
+
 
 Array.prototype.uniq = function() {
     let final = [];
@@ -19,7 +19,7 @@ Array.prototype.twoSum = function() {
     // debugger
     let final = [] 
     for (let i = 0; i < this.length; i++){
-        for (j = i + 1; j < this.length; j++) {
+        for (let j = i + 1; j < this.length; j++) {
             if (this[i] + this[j] === 0)
             final.push([i,j])
         };
@@ -27,4 +27,76 @@ Array.prototype.twoSum = function() {
     return final
 };
 
-console.log([-1, 0, 2, -2, 1].twoSum()) // => [[0,4] [2,3]]
+// console.log([-1, 0, 2, -2, 1].twoSum()) // => [[0,4] [2,3]]
+
+
+Array.prototype.transpose = function() {
+    let final = [] 
+    for (let i = 0; i < this.length; i++){
+        let subArray = [] 
+        for (let j = 0; j < this.length; j++) {
+            subArray.push(this[j][i])
+        };
+        final.push(subArray)
+    };
+    return final
+};
+
+// console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose()) // => [[0,3,6], [1,4,7], [2,5,8]]
+
+
+Array.prototype.myEach = function(callback) {
+    
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i])
+    }
+}
+
+function timesTwo(num) {
+     return num * 2
+}
+
+// console.log([1,2,3].myEach(timesTwo))
+
+Array.prototype.myMap = function(callback) {
+    let final = [];
+this.myEach(function (ele) {
+    final.push(callback(ele))
+})
+    // for (let i = 0; i < this.length; i++) {
+       
+    return final;
+}
+
+// console.log([2,1,3,4].myMap(timesTwo))
+
+Array.prototype.myReduce = function(callback, initialValue) {
+
+    const arrayCopy = this.slice() 
+    
+    if (initialValue !== undefined){
+        arrayCopy.unshift(initialValue)
+    }
+    let acc = arrayCopy.shift()
+    arrayCopy.myEach(function (ele) {
+        acc = callback(acc, ele)
+    })
+    return acc
+
+    // let count = 0
+    // if (initialValue !== null) {
+    //     count += initialValue 
+    //     this.myEach(function (ele) {
+    //         count += callback(ele)
+    //     })
+    // }
+    // return count
+}
+
+console.log([1, 2, 3].myReduce(function(acc, el) {
+    return acc + el;
+  })); // => 6
+
+ console.log([1, 2, 3].myReduce(function(acc, el) {
+    return acc + el;
+  }, 25)); // => 31
