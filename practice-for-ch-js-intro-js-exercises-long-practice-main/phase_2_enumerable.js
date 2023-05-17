b7b3c1d2ae8b7b55404c14fdc933691c2c8cdbe3
a-wong-8 <awong88@berkeley.edu>
2023-05-16 17:01:16 -0700

    .

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_2_enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_2_enumerable.js
@@ -1,1 +1,57 @@
 // Your code here
+
+Array.prototype.myEach = function(callback) {
+    
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i])
+    }
+}
+
+function timesTwo(num) {
+     return num * 2
+}
+
+// console.log([1,2,3].myEach(timesTwo))
+
+Array.prototype.myMap = function(callback) {
+    let final = [];
+this.myEach(function (ele) {
+    final.push(callback(ele))
+})
+    // for (let i = 0; i < this.length; i++) {
+       
+    return final;
+}
+
+// console.log([2,1,3,4].myMap(timesTwo))
+
+Array.prototype.myReduce = function(callback, initialValue) {
+
+    const arrayCopy = this.slice() 
+    
+    if (initialValue !== undefined){
+        arrayCopy.unshift(initialValue)
+    }
+    let acc = arrayCopy.shift()
+    arrayCopy.myEach(function (ele) {
+        acc = callback(acc, ele)
+    })
+    return acc
+
+    // let count = 0
+    // if (initialValue !== null) {
+    //     count += initialValue 
+    //     this.myEach(function (ele) {
+    //         count += callback(ele)
+    //     })
+    // }
+    // return count
+}
+
+// console.log([1, 2, 3].myReduce(function(acc, el) {
+//     return acc + el;
+//   })); // => 6
+
+//  console.log([1, 2, 3].myReduce(function(acc, el) {
+//     return acc + el;
+//   }, 25)); // => 31
--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_3_iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_3_iteration.js
@@ -1,1 +1,32 @@
 // Your code here
+
+Array.prototype.bubblesort = function() {
+    let sorted = false 
+    while (sorted === false) {
+        sorted = true
+        for (let i = 0; i < this.length - 1; i++) {
+            if (this[i] > this[i + 1]) {
+                sorted = false
+                // [this[i], this[i+1]] = [this[i+1], this[i]] 
+                firstNum = this[i]
+                secondNum = this[i + 1]
+                this[i] = secondNum
+                this[i + 1] = firstNum
+            }
+        }
+    } 
+    return this 
+}
+
+// console.log([2,3,5,1,2].bubblesort())
+
+String.prototype.substrings = function() {
+    let final = []
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length - 1; j++) {
+            final.push(i, j)
+        }
+    }
+}
+
+console.log('catman'.substrings()); 
